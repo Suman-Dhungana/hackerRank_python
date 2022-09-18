43e7d7965993a97e33e9219df495b1973eac6a54
Suman-Dhungana <sumandhungana85@gmail.com>
2022-09-18 16:29:36 +0545

    First release of Finding Sub String

---
+++ findSubString.py
@@ -0,0 +1,13 @@
+def count_substring(string, sub_string):
+    new=[]
+    sub_len = len(sub_string)
+    for n in range(len(string)):
+        if string[n:n+sub_len] == sub_string and len(string[n:n+sub_len])== sub_len:
+            new.append(n)
+    return (len(new))
+
+
+string = input("Enter Test String: ").strip()
+sub_string = input("Enter Sub String: ").strip()
+count = count_substring(string, sub_string)
+print(count)
