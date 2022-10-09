d84ad4df7153a0fc87a4c8afb4586b47f42f0ee7
Suman-Dhungana <sumandhungana85@gmail.com>
2022-10-09 16:57:10 +0545

    Cartesian Product

--- itertools.product().py
+++ itertools.product().py
@@ -1,16 +1,13 @@
 A=input()
-listA=list(A.split())
+listA=A.split()
 B=input()
-listB=list(B.split())
+listB=B.split()
 ls=[]
 row=[]
 for x in listA:
     for y in listB:
         ls.append(x)
         ls.append(y)
-        row.append(ls)
+        row= '(%s)'% ', '.join(map(str,ls))
+        print(row, end=' ')
         ls=[]
-    tup=tuple(row)
-tup='%s' % ', '.join(map(str,tup))
-# print(tup,sep=' ')
-print(''.join(tup))
