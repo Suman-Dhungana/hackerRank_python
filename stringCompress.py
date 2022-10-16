74d1015e0a888f2aaf62f8412c05f22f9edb0cd0
Suman-Dhungana <sumandhungana85@gmail.com>
2022-10-16 21:51:16 +0545

    Modified print statement

--- stringCompress.py
+++ stringCompress.py
@@ -5,5 +5,5 @@ compress=[]
 for k,g in groupby(x):
     compress.append(len(list(g)))
     compress.append(k)
-    print('(%s)' % ', '.join(map(str,keyLen)),end=" ")
+    print('(%s)' % ', '.join(map(str,compress)),end=" ")
     compress=[]
